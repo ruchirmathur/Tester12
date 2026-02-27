cf9a38421aa630e493bef9d3221184ead5d4cd2d
ruchirmathur <ruchirmat@gmail.com>
2026-02-27 01:56:26 -0800

    Add frontend/src/styles/global.ts

---
+++ frontend/src/styles/global.ts
@@ -0,0 +1,7 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * { box-sizing: border-box; }
+  body { margin:0; font-family: Inter, ui-sans-serif, system-ui, -apple-system, 'Segoe UI', Roboto, 'Helvetica Neue', Arial; }
+  a { text-decoration:none }
+`
